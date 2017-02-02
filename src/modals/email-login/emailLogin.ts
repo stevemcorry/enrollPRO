7cb3ae7467482630535d535a5d4d814ef156fb91
Stephen Corry <stevemcorry@gmail.com>
2017-02-02 14:15:16 -0700

    email login style and api call attempts

--- src/modals/email-login/emailLogin.ts
+++ src/modals/email-login/emailLogin.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/postService';
 import { GetService } from '../../services/getService';
 import { ModalController, Platform, NavParams, ViewController, NavController} from 'ionic-angular';
 import { TabsPage } from '../../pages/tabs/tabs';
@@ -6,10 +7,10 @@ import { TabsPage } from '../../pages/tabs/tabs';
 @Component({
   selector: 'page-email-login',
   templateUrl: 'emailLogin.html',
-  providers: [ GetService ]
+  providers: [ PostService, GetService]
 })
 export class EmailLoginModal implements OnInit{
-    constructor(public viewCtrl: ViewController, public navCtrl: NavController, public platform: Platform, public params: NavParams,){
+    constructor(public viewCtrl: ViewController, public navCtrl: NavController, public platform: Platform, public params: NavParams, public postService: PostService, public gettService: GetService){
     }
 
     login() {
@@ -18,7 +19,36 @@ export class EmailLoginModal implements OnInit{
     dismiss() {
         this.viewCtrl.dismiss();
     }
-    ngOnInit(){
+
+    public user = {
+    "grant_type": "password",
+    "client_id": "2",
+    "client_secret": "FxeeeKA65iEFExJWm0l08O6MJua8MTOoxiWz2sPZ",
+    "username": "bob@gmail.com",
+    "password": "111111",
+    "scope": ""
+    }
+    public newUser;
+    toURL(){
+        this.newUser = encodeURIComponent(JSON.stringify(this.user))
+    }
+    send(){
+        console.log(this.newUser);
+        this.postService.requestOAuth(this.newUser).subscribe(res=>{
+            console.log(res);
+        })
     }
     
+
+    get(){
+        this.gettService.getContacts().subscribe(res => {
+            console.log(res);
+        })
+    }
+
+
+    ngOnInit(){
+        this.toURL()
+    }
+
 }
